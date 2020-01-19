fixed bugs of covariance matrix

applyGeometricTransformation.py
@@ -42,13 +42,14 @@ def applyGeometricTransformation(startXs, startYs, newXs, newYs, bbox, Cov=None,
             # convert the variables to Homogeneous coordinate system
             cov = np.zeros((6, 6))
             cov[:2, :2] = Cov[obj_idx][:2, :2]
-            cov[-2:, -2:] = Cov[obj_idx][2:, 2:]
+            cov[-3:-1, -3:-1] = Cov[obj_idx][2:, 2:]
             A = np.zeros((6, 6))
             A[:3, :3] = mat
             A[3:, 3:] = mat
             cov = A.dot(cov).dot(A.T) + R
             Cov[obj_idx][:2, :2] = cov[:2, :2]
-            Cov[obj_idx][2:, 2:] = cov[-2:, -2:]
+            Cov[obj_idx][2:, 2:] = cov[-3:-1, -3:-1]
+
 
         """
         # estimate the new bounding box with only the inliners (Added by Yongyi Wang)

estimateAllTranslation.py
@@ -22,7 +22,10 @@ def estimateAllTranslation(startXs,startYs,img1,img2):
     newYs = np.full(startYs_flat.shape,-1,dtype=float)
     for i in range(np.size(startXs)):
         if startXs_flat[i] != -1:
-            newXs[i], newYs[i] = estimateFeatureTranslation(startXs_flat[i], startYs_flat[i], Ix, Iy, img1, img2)
+            try:
+                newXs[i], newYs[i] = estimateFeatureTranslation(startXs_flat[i], startYs_flat[i], Ix, Iy, img1, img2)
+            except np.linalg.LinAlgError:
+                None
     newXs = np.reshape(newXs, startXs.shape)
     newYs = np.reshape(newYs, startYs.shape)
     return newXs, newYs